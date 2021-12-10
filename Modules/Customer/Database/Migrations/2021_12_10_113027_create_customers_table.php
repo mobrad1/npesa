443060ca60fefe4a4fccf038c950205d362b32c6
mobrad1 <yarrowbradley@gmail.com>
2021-12-10 12:36:10 +0100

    Added Registeration Controller for customer module

--- Modules/Customer/Database/Migrations/2021_12_10_113027_create_customers_table.php
+++ Modules/Customer/Database/Migrations/2021_12_10_113027_create_customers_table.php
@@ -15,7 +15,30 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('email');
+            $table->bigInteger('balance')->default(0);
+            $table->string('account_tier');
+            $table->string('status');
+            $table->string('gender');
+            $table->string('profile_picture');
+            $table->string('signature');
+            $table->boolean('push_notification_settings');
+            $table->boolean('sms_notification_settings');
+            $table->string('marital_status');
+            $table->string('date_of_birth');
+            $table->string('state');
+            $table->string('area');
+            $table->string('city');
+            $table->dateTime('approved_at');
+            $table->dateTime('last_login');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number')->unique();
+            $table->timestamp('phone_number_verified_at');
+            $table->string('pin');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
