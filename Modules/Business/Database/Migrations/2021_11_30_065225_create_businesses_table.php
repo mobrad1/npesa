c17e6d0cc608da2b5f482d9e17fd16de577bdcaf
mobrad1 <yarrowbradley@gmail.com>
2022-01-13 19:57:51 +0100

    Added extra fields to business

--- Modules/Business/Database/Migrations/2021_11_30_065225_create_businesses_table.php
+++ Modules/Business/Database/Migrations/2021_11_30_065225_create_businesses_table.php
@@ -15,13 +15,11 @@ class CreateBusinessesTable extends Migration
     {
         Schema::create('businesses', function (Blueprint $table) {
             $table->id();
-
             // Required information for business
             $table->string('business_name');
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
-
             // Secondary information for business user
             $table->boolean('is_completed_owner_profile')->default(false);
             $table->string('first_name')->nullable();
@@ -29,7 +27,6 @@ class CreateBusinessesTable extends Migration
             $table->string('phone')->nullable();
             $table->boolean('is_registered')->default(false);
             $table->integer('team_size')->default(1);
-
             $table->timestamps();
         });
     }
